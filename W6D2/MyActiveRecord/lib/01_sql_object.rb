6ca1675dd5c55dc804f0b23161891a37056bf606
Victor Crispin <victorrcrispinjr@gmail.com>
2020-06-16 16:41:43 -0400

    Mid day update on SQL Object

--- W6D2/MyActiveRecord/lib/01_sql_object.rb
+++ W6D2/MyActiveRecord/lib/01_sql_object.rb
@@ -5,22 +5,51 @@ require 'active_support/inflector'
 
 class SQLObject
   def self.columns
-    # ...
+    return @columns if @columns
+    cols = DBConnection.execute2(<<-SQL).first
+      SELECT
+        *
+      FROM
+        #{self.table_name}
+      SQL
+    
+    cols.map!(&:to_sym)
+    @columns = cols
   end
 
   def self.finalize!
+    self.columns.each do |attribute|
+      #Getter
+      define_method(attribute) do
+        # self.instance_variable_get("@#{attribute}")
+        self.attributes[attribute]
+      end
+      
+      #Setter
+      define_method("#{attribute}=") do |options|
+      # self.instance_variable_set("@#{attribute}", options)
+      self.attributes[attribute] = options
+      end
+    end
   end
 
   def self.table_name=(table_name)
-    # ...
+    @table_name = table_name
   end
 
+
   def self.table_name
-    # ...
+    "#{self}s".downcase 
   end
 
   def self.all
-    # ...
+    query = DBConnection.execute(<<-SQL)
+      SELECT
+        *
+      FROM
+        #{table_name}
+    SQL
+    query
   end
 
   def self.parse_all(results)
@@ -32,11 +61,21 @@ class SQLObject
   end
 
   def initialize(params = {})
-    # ...
+    params.each do |attribute, value|
+
+      attr_sym = attribute.to_sym
+      columns = self.class.columns
+
+      if columns.include?(attr_sym)
+        self.send("#{attr_sym}=", value)
+      else
+        raise "unknown attribute '#{attr_sym}'" 
+      end
+    end
   end
 
   def attributes
-    # ...
+    @attributes ||= {}
   end
 
   def attribute_values
