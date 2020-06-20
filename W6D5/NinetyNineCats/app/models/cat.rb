30377a9a9ec7d1fdebbde7e5961223eb660f7aae
Victor Crispin <victorrcrispinjr@gmail.com>
2020-06-20 13:09:01 -0400

    Updated gemfile to add annotate gem, updated cat model

--- W6D5/NinetyNineCats/app/models/cat.rb
+++ W6D5/NinetyNineCats/app/models/cat.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: cats
+#
+#  id          :bigint           not null, primary key
+#  name        :string           not null
+#  color       :string           not null
+#  sex         :string(1)        not null
+#  description :text             not null
+#  birth_date  :date             not null
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
 class Cat < ApplicationRecord
     validates :
 end
